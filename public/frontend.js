771e5f41f32cf78a1a93cd7671891062abb0cf92
Juan A Cortes <juan.cortes@gmail.com>
2016-12-12 00:58:02 -0500

    Add pen width option.  Clean some of the code

--- public/frontend.js
+++ public/frontend.js
@@ -1,40 +1,25 @@
 var socket = io();
-
-// initialize on single element with element
-// var elem = document.getElementById('color-input');
-
-// use selector string to initialize on single element
-// more information can be found at http://www.jqueryrain.com/?HXLLAi6v and https://www.npmjs.com/package/huebee
-var hueb = new Huebee("#color-input", {
-  // options
-  setBGColor: true,
-  saturations: 2,
-  customColors: [ '#C25', '#E62', '#EA0', '#19F', '#333' ]
+socket.on("draw-together", function(coordinates) {
+  draw(coordinates);
 });
 
 const canvas = document.getElementById("draw-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 
-var eraser = document.getElementById("eraser");
-var clearPage = document.getElementById("clear-page");
-
-penColor = "#000";
+var penColor = "#000";
+var penWidth = 10;
 // ctx.lineJoin = "round";
 // ctx.lineCap = "round";
-// ctx.lineWidth = 10;
 
 var isDrawing = false;
 var lastMousePosition;
-
-document.getElementById("color-input").innerHTML = penColor;
-document.getElementById("color-input").style.background = penColor;
 
 function draw(coordinates) {
   if (coordinates.penColor === "white") {
     ctx.lineWidth = 25;
   } else {
-    ctx.lineWidth = 10;
+    ctx.lineWidth = coordinates.penWidth;
   }
   ctx.strokeStyle = coordinates.penColor;
   ctx.lineJoin = "round";
@@ -48,25 +33,47 @@ function draw(coordinates) {
   ctx.stroke();
 }
 
-socket.on("draw-together", function(coordinates) {
-  draw(coordinates);
+///////////////////////////////////
+// Start drawing options section //
+///////////////////////////////////
+// use selector string to initialize on single element
+// more information can be found at http://www.jqueryrain.com/?HXLLAi6v and https://www.npmjs.com/package/huebee
+var hueb = new Huebee("#color-input", {
+  // options
+  setBGColor: true,
+  saturations: 2,
+  customColors: [ '#C25', '#E62', '#EA0', '#19F', '#333' ]
 });
 
-//////////////////////////////
-// Start pen color section //
-/////////////////////////////
+document.getElementById("color-input").innerHTML = penColor;
+document.getElementById("color-input").style.background = penColor;
+var eraser = document.getElementById("eraser");
+var clearPage = document.getElementById("clear-page");
+
 // get color from button with id color-input (initialized as hueb)
 penColor = hueb.on("change", function(color) {
   penColor = color;
 });
 
+var pen_width = document.getElementById("pen-width");
+pen_width.addEventListener("change", function(event) {
+  penWidth = event.srcElement.value;
+});
+
 eraser.addEventListener("click", function(event) {
   penColor = event.srcElement.value;
 });
-////////////////////////////
-// End pen color section //
-///////////////////////////
 
+clearPage.addEventListener("click", function() {
+  location.reload();
+});
+/////////////////////////////////
+// End drawing options section //
+/////////////////////////////////
+
+///////////////////////////////////
+// Start event listeners section //
+///////////////////////////////////
 canvas.addEventListener("mousedown", function(event) {
   isDrawing = true;
 });
@@ -76,11 +83,11 @@ canvas.addEventListener("mousemove", function(event) {
     x : event.offsetX,
     y : event.offsetY
   };
-  // lastMousePosition = currentCoordinates;
   coordinates = {
     currentCoordinates : currentCoordinates,
     lastMousePosition : lastMousePosition,
-    penColor : penColor
+    penColor : penColor,
+    penWidth : penWidth
   };
   if (isDrawing === true) {
     if (lastMousePosition) {
@@ -98,7 +105,6 @@ canvas.addEventListener("mouseup", function(event) {
 canvas.addEventListener("mouseout", function() {
   isDrawing = false;
 });
-
-clearPage.addEventListener("click", function() {
-  location.reload();
-});
+/////////////////////////////////
+// End event listeners section //
+/////////////////////////////////
